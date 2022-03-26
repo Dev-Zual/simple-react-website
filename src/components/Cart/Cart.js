6f76ae6eb44386b7223c748f2c129df84b15c571
Dev-Zual <freelancerzual@gmail.com>
2022-03-26 15:27:30 +0600

    click to remove all items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
 const Cart = ({ item }) => {
-  console.log(item);
+  //   console.log(item);
   const { name, picture } = item;
   return (
-    <div className="flex items-center mt-3 bg-red-400 py-2 px-2 rounded-3xl">
-      <img className="w-10 rounded-full h-10 mr-2" src={picture} alt="" />
-      <h1 className="text-white font-semibold ml-2 text-lg">{name}</h1>
+    <div className="grid grid-cols-6 items-center mt-3 bg-red-400 py-2 px-2 rounded-3xl">
+      <div className="col-span-1">
+        <img className="w-10 rounded-full h-10 mr-2" src={picture} alt="" />
+      </div>
+      <div className="col-span-4">
+        <h1 className="text-white font-semibold ml-2 text-sm">{name}</h1>
+      </div>
+      <div className="col-span-1"></div>
     </div>
   );
 };
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -6,12 +6,19 @@ const Products = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState([]);
   //   console.log(cart);
+  // add to cart function
   const addToCart = (product) => {
     const newCart = [...cart, product];
     if (newCart.length > 4) {
+      alert('You can not select more then four items.');
     } else {
       setCart(newCart);
     }
+  };
+  // functio to remove all item
+  const removeItem = () => {
+    const emtyCart = [];
+    setCart(emtyCart);
   };
 
   useEffect(() => {
@@ -20,7 +27,7 @@ const Products = () => {
       .then((data) => setProducts(data));
   }, []);
   return (
-    <div className="grid grid-cols-12 w-5/6 m-auto gap-4">
+    <div className="grid grid-cols-12 w-11/12 m-auto gap-5">
       <div className="col-span-9 ">
         <div className="grid md:grid-cols-3 gap-4 lg:grid-cols-3 sm:grid-cols-1 mt-12">
           {products.map((product) => (
@@ -28,11 +35,21 @@ const Products = () => {
           ))}
         </div>
       </div>
-      <div className="col-span-3">
-        <h2>Selected Items</h2>
+      <div className="col-span-3 mt-10">
+        <h2 className="font-semibold text-2xl">Selected Items</h2>
         {cart.map((item) => (
           <Cart key={item.id} item={item}></Cart>
         ))}
+        <div>
+          <button className="bg-green-600 text-white py-2 px-4 rounded-md mt-5">
+            Chose One For Me
+          </button>
+        </div>
+        <div onClick={removeItem} className="mt-3">
+          <button className="bg-red-600 text-white py-2 px-4 rounded-md w-40">
+            Remove Item
+          </button>
+        </div>
       </div>
     </div>
   );
