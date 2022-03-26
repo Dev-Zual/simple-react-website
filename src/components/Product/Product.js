be86b73863af324ce0764463f8f3851f8fdb156a
Dev-Zual <freelancerzual@gmail.com>
2022-03-26 11:25:34 +0600

    create single pd componet & pass data

---
+++ src/components/Product/Product.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Product = () => {
+  return (
+    <div>
+      <h1> this is single product</h1>
+    </div>
+  );
+};
+
+export default Product;
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
 
 const Products = () => {
   const [products, setProducts] = useState([]);
@@ -11,10 +12,12 @@ const Products = () => {
     <div className="grid grid-cols-12 w-5/6 m-auto">
       <div className="col-span-9">
         <h2>this is Products.</h2>
+        {products.map((product) => (
+          <Product product={product} />
+        ))}
       </div>
       <div className="col-span-3">
         <h2>cart container</h2>
-        {products.map((product) => console.log(product))}
       </div>
     </div>
   );
