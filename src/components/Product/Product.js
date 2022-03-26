20be3e2daed4f19ee82ca9987fc3c1a27367a9fd
Dev-Zual <freelancerzual@gmail.com>
2022-03-26 12:06:09 +0600

    show all data in cart

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,9 +4,16 @@ const Product = ({ product }) => {
   console.log(product);
   const { name, company, picture, price } = product;
   return (
-    <div className="shadow-md border-2 p-3">
+    <div className="shadow-md border-2 p-3 rounded-md">
       <img className="w-11/12 mx-auto" src={picture} alt="" />
-      <h1> {name}</h1>
+      <h3 className="text-lg font-semibold mt-2"> {name}</h3>
+      <h3 className="text-base font-semibold mt-2">
+        Company: <span className="font-bold">{company}</span>
+      </h3>
+      <h3 className="text-base font-semibold mt-2">
+        Price:
+        <small className="text-sm pl-2 font-bold">{price}</small>{' '}
+      </h3>
     </div>
   );
 };
